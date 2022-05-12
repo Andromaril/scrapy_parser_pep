c430b4d357e5ccebf846afc773908c19de5c5dab
Andronik Maria <andronikmar@yandex.com>
2022-05-12 20:45:45 +0300

    parse pep8

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,39 @@
+import csv
+import datetime as dt
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+
+
+class PepParsePipeline:
+
+    def open_spider(self, spider):
+        self.counts = {'Active': 0, 'Final': 0, 'Superseded': 0,
+                       'Deferred': 0, 'Rejected': 0,
+                       'Withdrawn': 0, 'April Fool!': 0,
+                       'Draft': 0, 'Final': 0, 'Accepted': 0, }
+
+    def process_item(self, item, spider):
+        status = item['status']
+        self.counts[status] = self.counts.get(status) + 1
+        return item
+
+    def close_spider(self, spider):
+
+        downloads_dir = BASE_DIR / 'results'
+        now_date = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        file = f'status_summary_{now_date}.csv'
+        path = downloads_dir / file
+        with open(path, mode='w', encoding='utf-8') as csvfile:
+            header = ['Статус', 'Количество']
+            pep = self.counts.items()
+            total = ['Total', sum(self.counts.values())]
+            spamwriter = csv.writer(
+                csvfile,
+                quoting=csv.QUOTE_MINIMAL
+            )
+            spamwriter.writerows([
+                header,
+                *pep,
+                total,
+            ])
---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,27 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
+
+    def parse(self, response):
+
+        all_peps = response.css('a.reference.external::attr(href)')
+        for pep in all_peps:
+            yield response.follow(pep, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        path = '//*[@id="pep-page-section"]/header/ul/li[3]//text()'
+        number = [x.replace('PEP ', '').strip()
+                  for x in response.xpath(path).extract()]
+        data = {
+            'number': number,
+            'name': response.css('h1.page-title::text').get(),
+            'status': response.css('dt:contains("Status") + dd::text').get()
+        }
+        yield PepParseItem(data)
